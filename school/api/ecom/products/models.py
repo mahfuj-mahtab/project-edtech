3d7e7a1a25aee1ee65a34abc24378153fc651f92
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2024-05-05 15:08:00 +0600

    working on ecommerce products

---
+++ school/api/ecom/products/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    stock_quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    sku = models.CharField(max_length=50, unique=True)
+    weight = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    length = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    width = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    height = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
